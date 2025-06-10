b55f41dfe9f17890e2a902df12b67ef432c3f7c1
Arslan-MD <157588592+ArslanMDofficial@users.noreply.github.com>
2025-06-11 00:19:19 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
 module.exports = {
   PREFIX: process.env.PREFIX || "Arslan-MD",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
+  EMAIL: process.env.EMAIL || "majexad226@2mik.com",
+  PASS: process.env.PASS || "majexad226@",
   MESSAGE: process.env.MESSAGE || "Thank you for using our Arslan-MD bot dont share your session id",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
